aa16a835a53990a2e7b4cf50495f3545099d7b52
Kenneth Rodriguez <kennethrdz@hotmail.com>
2022-02-27 16:08:23 -0600

    Setup script

---
+++ my_script.bash
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+sudo apt-get update
+sudo apt install openjdk-8-jdk
+sudo apt install openjdk-11-jdk
+sudo apt install openjdk-17-jdk
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
+sudo update-alternatives --config jar
+sudo apt install trash
+sudo apt install ffmpeg
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install obs-studio
+sudo ubuntu-drivers autoinstall
+sudo apt install printer-driver-escpr
+sudo apt install zip
+sudo snap install flutter --classic
+sudo apt install vi
+git config --global user.name
+git config --global user.email
+sudo apt-get install build-essential
+sudo apt-get update
