a6e3f3e55c498efa278ed665cd5f978843b6a068
KennethRdz <78935468+KBlacksmith@users.noreply.github.com>
2022-02-27 19:33:08 -0600

    Actualización mayor

--- my_script.bash
+++ my_script.bash
@@ -1,9 +1,13 @@
 #! /bin/sh
+#Basic Commands
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo ubuntu-drivers autoinstall
 
-sudo apt-get update
+#Apt
 sudo apt install git
-git config --global user.name
-git config --global user.email
+git config --global user.name "Kenneth Rodriguez"
+git config --global user.email "kennethrdz@hotmail.com"
 sudo apt install openjdk-8-jdk
 sudo apt install openjdk-11-jdk
 sudo apt install openjdk-17-jdk
@@ -12,12 +16,39 @@ sudo update-alternatives --config javac
 sudo update-alternatives --config jar
 sudo apt install trash-cli
 sudo apt install ffmpeg
-sudo add-apt-repository ppa:obsproject/obs-studio
-sudo apt update
 sudo apt install obs-studio
-sudo ubuntu-drivers autoinstall
 sudo apt install printer-driver-escpr
-sudo snap install flutter --classic
-sudo apt-get install build-essential
+sudo apt install build-essential
 sudo apt install zip
+sudo apt install gparted
+sudo apt install pdfarranger
+sudo apt install codeblocks
+sudo apt install idle
+
+#Snap
+sudo snap install flutter --classic
+sudo snap install spotify
+sudo snap install shortwave
+sudo snap install vlc
+sudo snap install android-studio --classic
+
+#Browsing
+browse https://github.com/nvm-sh/nvm#install--update-script
+browse https://github.com/denoland/deno_install
+browse https://zoom.us/download
+broswe https://www.typescriptlang.org/download
+browse https://www.google.com/intl/en/chrome/
+browse https://store.steampowered.com/about/
+browse https://www.openshot.org/
+browse https://krita.org
+browse https://godotengine.org/download
+browse https://www.audacityteam.org/download/linux/
+browse https://netbeans.apache.org/
+browse https://docs.microsoft.com/en-us/sql/linux
+browse https://ionicframework.com/
+browse https://angular.io/
+browse https://unity.com/download
+browse https://www.rust-lang.org/
+browse https://deno.land/#installation
+
 sudo apt-get autoremove
